77b8c22238fc2728b53ad5e58720e5ea8353f127
Ariel Turchinsky <ariel7590@gmail.com>
2024-08-22 03:20:37 +0300

    blabla

--- client/src/components/navbar/navbar.tailwind.ts
+++ client/src/components/navbar/navbar.tailwind.ts
@@ -1,6 +1,6 @@
 export const navbarContainer =
-	"bg-[black] shadow-[2px_2px_2px_#747272] flex justify-between items-center px-2.5 py-0";
+	"bg-[black] flex justify-between items-center px-2.5 py-2";
 
 export const welcomeUser = "flex items-center";
-export const signIn = "cursor-pointer text-white";
+export const signIn = "cursor-pointer text-white min-w-[90px]";
 export const logo = "text-white";
