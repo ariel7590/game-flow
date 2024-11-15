a37b90eb6148868f6a53e6a6ea0048edcc7d8a48
ariel7590 <ariel7590@gmail.com>
2024-11-15 10:19:33 +0200

    pretty much finished styling the ai results

--- client/src/components/ai-guide/ai-guide.component.tsx
+++ client/src/components/ai-guide/ai-guide.component.tsx
@@ -6,57 +6,83 @@ import ReactMarkdown from "react-markdown";
 import CircularProgress from "@mui/material/CircularProgress";
 
 interface aiGuideProps {
-	prompt: string;
+  prompt: string;
 }
 const AiGuide = ({ prompt }: aiGuideProps) => {
-	const guide = useSelector((state: RootState) => state.aiGuide);
-	const dispatch = useDispatch<AppDispatch>();
+  const guide = useSelector((state: RootState) => state.aiGuide);
+  const dispatch = useDispatch<AppDispatch>();
 
-	useEffect(() => {
-		const dispatchGuide = async () => {
-			await dispatch(getGeneratedAnswer(prompt));
-		};
+  useEffect(() => {
+    const dispatchGuide = async () => {
+      await dispatch(getGeneratedAnswer(prompt));
+    };
 
-		dispatchGuide();
-	}, []);
+    dispatchGuide();
+  }, []);
 
-	const loading = (
-		<div className='flex justify-center mt-[20%]'>
-			<CircularProgress size='4rem' className='text-[#f09b1c]' />
-		</div>
-	);
+  const loading = (
+    <div className="flex justify-center mt-[20%]">
+      <CircularProgress size="4rem" className="text-[#f09b1c]" />
+    </div>
+  );
 
-	const answer = (
-		<div className='flex flex-col items-center p-[50px]'>
-				<ReactMarkdown
-					children={guide.answer}
-					components={{
-						h1: ({ node, ...props }) => (
-							<h1 className='font-bold my-4 text-[1.5em]' {...props} />
-						),
-						h2: ({ node, ...props }) => (
-							<h2 className='font-bold my-4 text-[1.5em]' {...props} />
-						),
-						h3: ({ node, ...props }) => (
-							<h3 className='font-bold my-4' {...props} />
-						),
-						h4: ({ node, ...props }) => (
-							<h4 className='font-bold my-3' {...props} />
-						),
-						li: ({ node, ...props }) => (
-							<li className='self-start' {...props} />
-						),
-						ol: ({ node, ...props }) => (
-							<ol className='list-[circle]' {...props} />
-						),
-						ul: ({ node, ...props }) => <ul className='mb-3' {...props} />,
-						p: ({ node, ...props }) => <p className='my-2' {...props} />,
-					}}
-				/>
-		</div>
-	);
+  const answer = (
+    <div className="flex flex-col items-center p-[50px]">
+      <div className="w-[50%]">
+        <ReactMarkdown
+          children={guide.answer}
+          components={{
+            h1: ({ node, ...props }) => (
+              <h1 className="font-bold my-4 text-[1.5em]" {...props} />
+            ),
+            h2: ({ node, ...props }) => (
+              <h2 className="font-bold my-4 text-[1.5em]" {...props} />
+            ),
+            h3: ({ node, ...props }) => (
+              <h3 className="font-bold my-4" {...props} />
+            ),
+            h4: ({ node, ...props }) => (
+              <h4 className="font-bold my-3" {...props} />
+            ),
+            li: ({ node, ...props }) => {
+              if (node?.children) {
+                let listStyle = "";
+                node.children.map((child) => {
+                  if (child.type === "element" && child.tagName === "strong") {
+                    listStyle = "list-['-']";
+                  } else if (
+                    child.type === "element" &&
+                    child.tagName !== "strong"
+                  ) {
+                    listStyle = "";
+                  } else if (
+                    child.type === "element" &&
+                    child.tagName === "p"
+                  ) {
+                    listStyle = "list-[none]";
+                  } else if (child.type === "text") {
+                    listStyle = "list-[disc]";
+                  }
+                });
 
-	return <>{guide.loading ? loading: answer}</>;
+                return (
+                  <li
+                    className={`self-start mb-[8px] ${listStyle}`}
+                    {...props}
+                  />
+                );
+              }
+            },
+            ol: ({ node, ...props }) => <ol className="mb-3" {...props} />,
+            ul: ({ node, ...props }) => <ul className="mb-3" {...props} />,
+            p: ({ node, ...props }) => <p className="my-2 list-[none]" {...props} />,
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  return <>{guide.loading ? loading : answer}</>;
 };
 
 export default AiGuide;
